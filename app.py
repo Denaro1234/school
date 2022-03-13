8c4cec82e29ac8f91c2a9ed60986bfe8f16f9752
Denaro1234 <98958796+Denaro1234@users.noreply.github.com>
2022-03-13 13:07:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('St.John Paul II Cambrige College ')
 countries = ['Information', 'people', 'about', 'Contact']
-st.image(f"https://flagcdn.com/80x60/{country_code[country]}.png")
+st.image(f"https://jp2college.com/wp-content/uploads/2020/10/How-we-started-3-1.jpg")
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.sidebar.write('select data type')
