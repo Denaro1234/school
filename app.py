968bc90a6436270df829e2e2ba6f2204185181f1
Denaro1234 <98958796+Denaro1234@users.noreply.github.com>
2022-03-13 12:55:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-Import streamlit as st
+import streamlit as st
 
 st.title('St.John Paul II Cambrige College ')
+
+st.sidebar.write('select data type')
 country = st.sidebar.selectbox('Select Country', countries)
+
